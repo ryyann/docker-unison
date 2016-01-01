19ab12cb5b047589155ac91bf0357a38959fa9de
Ryan Carey <r.carey@wks-170.corp.pao.dailymotion.com>
2015-12-31 18:01:48 -0800

    update to use native fswatch

--- unison-install.sh
+++ unison-install.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-cd /tmp/unison
+cd /tmp/unison || exit
 for UNISON_TAR_GZ in `find *.tar.gz`
 do
-  cd /tmp/unison
-  tar -zxvf $UNISON_TAR_GZ
+  cd /tmp/unison || exit
+  tar -zxvf "$UNISON_TAR_GZ"
   UNISON_RELEASE=${UNISON_TAR_GZ%.tar.gz}
-  cd $UNISON_RELEASE
-  make UISTYLE=text
-  cp unison /bin/$UNISON_RELEASE
+  cd "$UNISON_RELEASE" || exit
+  make UISTYLE=text NATIVE=true
+  cp unison /bin/"$UNISON_RELEASE"
+  if [ -f unison-fsmonitor ]; then
+    cp unison-fsmonitor /bin/"$UNISON_RELEASE-fsmonitor"
+  fi
+  if [ -f /bin/unison-"$UNISON_VERSION"-fsmonitor ]; then
+  	ln -s -f /bin/unison-"$UNISON_VERSION"-fsmonitor /bin/unison-fsmonitor
+  fi
 done
